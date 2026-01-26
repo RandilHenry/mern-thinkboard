3576882b2a3fbd30b86a48f04cbe8f7771d0c26d
RandilHenry <randilhenry@gmail.com>
2026-01-26 10:44:39 +0530

    Modify server.js for Vercel serverless deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -37,19 +37,9 @@ app.use(rateLimiter);
 
 app.use("/api/notes", notesRoutes);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+connectDb();
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-    });
-}
-
-connectDb().then(() => {
-    app.listen(PORT, () => {
-        console.log('Server started on PORT:', PORT);
-    });
-})
+export default app;
 
 
 
