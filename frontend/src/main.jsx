2ba5510997ef35e36f7d379245af9a5223b1672a
RandilHenry <randilhenry@gmail.com>
2026-03-22 12:21:24 +0530

    Fix React Router basename for GitHub Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,7 @@ import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/mern-thinkboard/">
       <App />
       <Toaster />
     </BrowserRouter>
